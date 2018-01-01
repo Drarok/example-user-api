30c7e57b6aa2be19df53ab730bbc31166a14582c
Mat Gadd <mat@zerifas.com>
2018-01-01 18:55:59 +0000

    Improve test grouping

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -25,184 +25,194 @@ describe('Test users API', () => {
       .sync({ force: true });
   });
 
-  it('GET /users starts with empty array', () => {
-    return request(app)
-      .get('/users')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .expect([]);
+  describe('GET /users', () => {
+    it('starts with empty array', () => {
+      return request(app)
+        .get('/users')
+        .expect(200)
+        .expect([]);
+    });
+
+    it('returns all users', () => {
+      return Promise
+        .all(testUsers.map((u) => {
+          return models.User.create(u)
+        }))
+        .then(() => {
+          return request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+              assert.equal(res.body.length, 2, 'Incorrect user count');
+            });
+        });
+    });
   });
 
-  it('GET /users returns all users', () => {
-    return Promise
-      .all(testUsers.map((u) => {
-        return models.User.create(u)
-      }))
-      .then(() => {
-        return request(app)
-          .get('/users')
-          .expect(200)
-          .expect('Content-Type', /json/)
-          .expect((res) => {
-            assert.equal(res.body.length, 2, 'Incorrect user count');
-          });
-      });
-  });
+  describe('GET /users/:userId', () => {
+    it('returns 404 with no data', () => {
+      return request(app)
+        .get('/users/1')
+        .expect(404);
+    });
 
-  it('GET /users/:userId returns 404 with no data', () => {
-    return request(app)
-      .get('/users/1')
-      .expect(404);
-  });
+    it('returns valid user data', () => {
+      const user = testUsers[0];
 
-  it('GET /users/:userId returns a single user', () => {
-    const user = testUsers[0];
-
-    return models.User.create(user)
-      .then(() => {
-        return request(app)
-          .get('/users/1')
-          .expect(200)
-          .expect((res) => {
-            const errors = [];
-            for (let field in user) {
-              if (res.body[field] !== user[field]) {
-                errors.push(`${field} mismatch in response`);
+      return models.User.create(user)
+        .then(() => {
+          return request(app)
+            .get('/users/1')
+            .expect(200)
+            .expect((res) => {
+              const errors = [];
+              for (let field in user) {
+                if (res.body[field] !== user[field]) {
+                  errors.push(`${field} mismatch in response`);
+                }
               }
-            }
 
-            if (errors.length) {
-              throw new Error(errors.join(', '));
-            }
-          });
-      });
+              if (errors.length) {
+                throw new Error(errors.join(', '));
+              }
+            });
+        });
+    });
   });
 
-  it('POST /users creates a user', () => {
-    let user = testUsers[0];
+  describe('POST /users', () => {
+    it('creates a user', () => {
+      let user = testUsers[0];
 
-    return request(app)
-      .post('/users')
-      .send(user)
-      .expect(201)
-      .expect('Location', '/users/1')
-      .expect((res) => {
-        const errors = [];
-        for (let field in user) {
-          if (res.body[field] !== user[field]) {
-            errors.push(`${field} mismatch in response`);
+      return request(app)
+        .post('/users')
+        .send(user)
+        .expect(201)
+        .expect('Location', '/users/1')
+        .expect((res) => {
+          const errors = [];
+          for (let field in user) {
+            if (res.body[field] !== user[field]) {
+              errors.push(`${field} mismatch in response`);
+            }
           }
-        }
 
-        if (errors.length) {
-          throw new Error(errors.join(', '));
-        }
-      })
-      .expect(() => {
-        return models.User.findAll()
-          .then((users) => {
-            assert(users.length, 1, 'User does not exist in the database');
+          if (errors.length) {
+            throw new Error(errors.join(', '));
+          }
+        })
+        .expect(() => {
+          return models.User.findAll()
+            .then((users) => {
+              assert(users.length, 1, 'User does not exist in the database');
 
-            const errors = [];
-            for (let field in user) {
-              if (users[0][field] !== user[field]) {
-                errors.push(`${field} mismatch in database`);
+              const errors = [];
+              for (let field in user) {
+                if (users[0][field] !== user[field]) {
+                  errors.push(`${field} mismatch in database`);
+                }
               }
-            }
 
-            if (errors.lenth) {
-              throw new Error(errors.join(', '));
-            }
-          });
-      });
+              if (errors.lenth) {
+                throw new Error(errors.join(', '));
+              }
+            });
+        });
+    });
   });
 
-  it('PATCH /users/:userId returns 404 with no data', () => {
-    return request(app)
-      .patch('/users/1')
-      .send({})
-      .expect(404);
+  describe('PATCH /users/:userId', () => {
+    it('returns 404 with no data', () => {
+      return request(app)
+        .patch('/users/1')
+        .send({})
+        .expect(404);
+    });
+
+    it('updates only given data', () => {
+      const user = testUsers[0];
+      const update = {
+        surname: 'Smith',
+      };
+
+      const expected = {
+        email: 'mat@example.com',
+        forename: 'Mat',
+        surname: 'Smith',
+      };
+
+      return models.User.create(user)
+        .then(() => {
+          return request(app)
+            .patch('/users/1')
+            .send(update)
+            .expect(200)
+            .expect((res) => {
+              for (let field in expected) {
+                assert.equal(res.body[field], expected[field]);
+              }
+            });
+        });
+    });
   });
 
-  it('PATCH /users/:userId updates only given data', () => {
-    const user = testUsers[0];
-    const update = {
-      surname: 'Smith',
-    };
+  describe('PUT /users/:userId', () => {
+    it('returns 404 with no data', () => {
+      return request(app)
+        .put('/users/1')
+        .send({})
+        .expect(404);
+    });
 
-    const expected = {
-      email: 'mat@example.com',
-      forename: 'Mat',
-      surname: 'Smith',
-    };
+    it('updates all data', () => {
+      const user = testUsers[0];
+      const update = {
+        surname: 'Smith',
+      };
 
-    return models.User.create(user)
-      .then(() => {
-        return request(app)
-          .patch('/users/1')
-          .send(update)
-          .expect(200)
-          .expect((res) => {
-            for (let field in expected) {
-              assert.equal(res.body[field], expected[field]);
-            }
-          });
-      });
+      const expected = {
+        email: 'mat@example.com',
+        forename: null,
+        surname: 'Smith',
+      };
+
+      return models.User.create(user)
+        .then(() => {
+          return request(app)
+            .put('/users/1')
+            .send(update)
+            .expect(200)
+            .expect((res) => {
+              for (let field in expected) {
+                assert.equal(res.body[field], expected[field]);
+              }
+            });
+        });
+    });
   });
 
-  it('PUT /users/:userId returns 404 with no data', () => {
-    return request(app)
-      .put('/users/1')
-      .send({})
-      .expect(404);
-  });
+  describe('DELETE /users/:userId', () => {
+    it('returns 404 with no data', () => {
+      return request(app)
+        .delete('/users/1')
+        .expect(404);
+    });
 
-  it('PUT /users/:userId updates all data', () => {
-    const user = testUsers[0];
-    const update = {
-      surname: 'Smith',
-    };
+    it('deletes given user', () => {
+      const user = testUsers[0];
 
-    const expected = {
-      email: 'mat@example.com',
-      forename: null,
-      surname: 'Smith',
-    };
-
-    return models.User.create(user)
-      .then(() => {
-        return request(app)
-          .put('/users/1')
-          .send(update)
-          .expect(200)
-          .expect((res) => {
-            for (let field in expected) {
-              assert.equal(res.body[field], expected[field]);
-            }
-          });
-      });
-  });
-
-  it('DELETE /users/:userId returns 404 with no data', () => {
-    return request(app)
-      .delete('/users/1')
-      .expect(404);
-  });
-
-  it('DELETE /users/:userId deletes given user', () => {
-    const user = testUsers[0];
-
-    return models.User.create(user)
-      .then(() => {
-        return request(app)
-          .delete('/users/1')
-          .expect(204)
-          .expect(() => {
-            return models.User.findAll()
-              .then((users) => {
-                assert.equal(users.length, 0);
-              });
-          });
-      });
+      return models.User.create(user)
+        .then(() => {
+          return request(app)
+            .delete('/users/1')
+            .expect(204)
+            .expect(() => {
+              return models.User.findAll()
+                .then((users) => {
+                  assert.equal(users.length, 0);
+                });
+            });
+        });
+    });
   });
 });
